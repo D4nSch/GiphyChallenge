6d1e7546d0910e371abcc9436bf291b039c96e90
DanSch <daniel.schaberreiter@gmail.com>
2022-09-22 02:20:00 +0200

    add favorites logic to data-service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
-import { GiphyResponse, ReducedGiphyResponse } from '../models/giphyresponse';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { ReducedGif, ReducedGiphyResponse } from '../models/giphyresponse';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,25 @@ export class DataService {
   // ReplaySubject(1) doesn't have an initial value until it is set with next, but emits last value on subscription
   // private searchResult$: Subject<GiphyResponse> = new ReplaySubject<GiphyResponse>(1);
   private searchResults$: Subject<ReducedGiphyResponse> = new Subject<ReducedGiphyResponse>;
+  private favoriteGifs$: BehaviorSubject<ReducedGif[]> = new BehaviorSubject<ReducedGif[]>(JSON.parse(localStorage.getItem("favoriteGifsList")?.length ? localStorage.getItem("favoriteGifsList")! : "[]"));
+
+  getFavoriteGifs$(): Observable<ReducedGif[]> {
+    return this.favoriteGifs$.asObservable();
+  }
+
+  addFavoriteGif$(favoriteGif: ReducedGif) {
+    let newFavoriteGifsList = this.favoriteGifs$.getValue().concat(favoriteGif);
+
+    localStorage.setItem("favoriteGifsList", JSON.stringify(newFavoriteGifsList));
+    this.favoriteGifs$.next(this.favoriteGifs$.getValue().concat(favoriteGif));
+  }
+
+  removeFavoriteGif$(favoriteGif: ReducedGif) {
+    let newFavoriteGifsList = this.favoriteGifs$.getValue().filter(gifEntry => gifEntry.id !== favoriteGif.id);
+
+    localStorage.setItem("favoriteGifsList", JSON.stringify(newFavoriteGifsList));
+    this.favoriteGifs$.next(newFavoriteGifsList);
+  }
 
   getSearchResults$(): Observable<ReducedGiphyResponse> {
     return this.searchResults$.asObservable();
