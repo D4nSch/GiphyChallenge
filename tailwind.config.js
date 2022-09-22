7b10ba67fa861235c2053d0dc181c4d013a68f39
DanSch <daniel.schaberreiter@gmail.com>
2022-09-22 02:17:50 +0200

    add daisyUI elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    // default breakpoints but with 40px removed
+    screens: {
+      sm: '550px',
+      md: '700px',
+      lg: '900px',
+      xl: '1100px',
+      '2xl': '1300px',
+    },
+    colors: {
+      primary: '#5c6ac4',
+      secondary: '#ecc94b'
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: []
+  }
 }
