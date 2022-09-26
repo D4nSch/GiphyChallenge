3c199bd9035fbe9ccfa1eda7c0e44fb831cbb454
DanSch <daniel.schaberreiter@gmail.com>
2022-09-26 03:41:33 +0200

    setup clips component

--- src/app/clips/clips.component.ts
+++ src/app/clips/clips.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { NgxMasonryComponent } from 'ngx-masonry';
+import { Subject, takeUntil } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { ReducedData } from '../models/giphyresponse';
+import { DataService } from '../services/data.service';
+import { GiphyService } from '../services/giphy.service';
+import { LayoutUpdateService } from '../services/layout-update.service';
+import { LoaderService } from '../services/loader.service';
 
 @Component({
   selector: 'app-clips',
   templateUrl: './clips.component.html',
-  styleUrls: ['./clips.component.scss']
+  styleUrls: ['./clips.component.scss'],
+  providers: [GiphyService]
 })
 export class ClipsComponent implements OnInit {
+  @ViewChild('clips') masonry?: NgxMasonryComponent;
+  @ViewChildren('clipsListItems') clipsListItems?: QueryList<ElementRef>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('window:scroll')
+  onScroll() {
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+      // Prevent multiple loads
+      if(this.loaderService.isLoading.getValue() === false) {
+        this.giphyService.getNextItems("clips", environment.gTrendingClipsUrl);
+      }
+    }
   }
 
+  trendingResults$ = this.dataservice.getClipsResults$();
+  
+  private readonly destroy$ = new Subject<void>();
+
+  constructor(private giphyService: GiphyService, private dataservice: DataService, private loaderService: LoaderService, private layoutUpdateService: LayoutUpdateService) { }
+
+  ngOnInit(): void {
+    this.layoutUpdateService.getLayoutUpdateTrigger$()
+    .pipe(
+      takeUntil(this.destroy$)
+    )
+    .subscribe(async () => {
+      let tries = this.layoutUpdateService.tries;
+      let pauseTime = this.layoutUpdateService.pauseTime;
+
+      for (let index = 0; index < tries; index++) {
+        await new Promise(resolve => setTimeout(resolve, pauseTime)).then(() => this.fixLayout());
+      }
+    });
+
+    this.giphyService.getTrendingClips()
+    .pipe(
+      takeUntil(this.destroy$)
+    )
+    .subscribe((reducedTrendingResults) => {
+      this.dataservice.setClipsResults$(reducedTrendingResults);
+      this.layoutUpdateService.setLayoutUpdate$(true);
+    })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  setFavorite(reducedData: ReducedData): void {
+    this.dataservice.addFavoriteItem$(reducedData);
+    this.layoutUpdateService.setLayoutUpdate$(true);
+  }
+
+  playVideo(index: number): void {
+    if(this.clipsListItems !== undefined) {
+      let hoveredItem = this.clipsListItems.toArray()[index];
+
+      hoveredItem.nativeElement.muted = false;
+      hoveredItem.nativeElement.play();
+    }
+  }
+  
+  stopVideo(index: number): void {
+    if(this.clipsListItems !== undefined) {
+      let hoveredItem = this.clipsListItems.toArray()[index];
+      
+      hoveredItem.nativeElement.muted = true;
+      hoveredItem.nativeElement.pause();
+    }
+  }
+
+  // ngx-masonry seems to have trouble with undefined heights (overlapping)
+  fixLayout() {
+    if (this.masonry !== undefined) {
+      // this.masonry.reloadItems();
+      this.masonry.layout();
+    }
+  }
 }
