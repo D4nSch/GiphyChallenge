32a7d13bf0ab1fdf7208bc4e8075634a0f87678d
DanSch <daniel.schaberreiter@gmail.com>
2022-10-12 20:55:41 +0200

    remove obsolete comments

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,11 +11,9 @@ export class DataService {
   constructor(private notificationService: NotificationService) { }
 
   private searchQuery$ = new BehaviorSubject("");
-  // ReplaySubject(1) doesn't have an initial value until it is set with next, but emits last value on subscription
-  // private searchResult$: Subject<GiphyResponse> = new ReplaySubject<GiphyResponse>(1);
-  private searchResults$ = new Subject<ReducedGiphyResponse>;
-  private trendingResults$ = new Subject<ReducedGiphyResponse>;
-  private clipsResults$ = new Subject<ReducedGiphyResponse>;
+  private searchResults$ = new Subject<ReducedGiphyResponse>();
+  private trendingResults$ = new Subject<ReducedGiphyResponse>();
+  private clipsResults$ = new Subject<ReducedGiphyResponse>();
   
   private selectedItem$ = new Subject<ReducedData | undefined>;
 
@@ -38,7 +36,7 @@ export class DataService {
   
   addFavoriteItem$(favoriteItem: ReducedData) {
     let duplicateItemsList = this.favoriteItems$.getValue().filter(item => item.id === favoriteItem.id);
-    
+
     if(duplicateItemsList.length === 0) {
       let newFavoriteItemsList = this.favoriteItems$.getValue().concat(favoriteItem);
       
@@ -53,7 +51,7 @@ export class DataService {
   
   removeFavoriteItem$(favoriteItem: ReducedData) {
     let newFavoriteItemsList = this.favoriteItems$.getValue().filter(item => item.id !== favoriteItem.id);
-    
+
     localStorage.setItem("favoriteItemsList", JSON.stringify(newFavoriteItemsList));
     this.favoriteItems$.next(newFavoriteItemsList);
     
