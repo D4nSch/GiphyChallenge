b9474cbb334c76c4110d88455df57855b6871052
DanSch <daniel.schaberreiter@gmail.com>
2022-12-13 15:51:06 +0100

    add environment template

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    gApiKey: "XXXXX", // THIS API KEY NEEDS TO SUPPORT THE 'CLIPS' ENDPOINT
+    gSearchGifsUrl: "https://api.giphy.com/v1/gifs/search",
+    gTrendingGifsUrl: "https://api.giphy.com/v1/gifs/trending",
+    gTrendingClipsUrl: "https://api.giphy.com/v1/clips/trending",
+    layoutUpdateTries: 10,
+    layoutUpdatePauseTime: 150
+};
